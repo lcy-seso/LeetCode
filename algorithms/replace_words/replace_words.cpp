47569ddfdfa41b6162b128cc706f67bc5832e330
lcy-seso <lcy.seso@gmail.com>
2018-06-29 10:18:50 +0800

    update 648.

--- algorithms/replace_words/replace_words.cpp
+++ algorithms/replace_words/replace_words.cpp
@@ -7,46 +7,58 @@
 const int kAlphabetSize = 26;
 
 struct TrieNode {
-  TrieNode* children[kAlphabetSize];
-  bool isEndOfWord;
+  char val;
+  TrieNode* next[kAlphabetSize];
+  bool isEndOfWord = true;
 
-  TrieNode() {
-    for (int i = 0; i < kAlphabetSize; ++i) children[i] = NULL;
+  TrieNode(char c) : val(c) {
+    for (int i = 0; i < kAlphabetSize; ++i) next[i] = NULL;
     isEndOfWord = false;
-  };
-  ~TrieNode() {
-    for (int i = 0; i < kAlphabetSize; ++i) {
-      if (children[i]) delete children[i];
-    }
   };
 };
 
-void insertTrie(TrieNode* root, std::string key) {
+void insertTrie(TrieNode* root, const std::string key) {
   TrieNode* cur = root;
   for (int i = 0; i < key.length(); ++i) {
     int index = key[i] - 'a';
 
-    if (!cur->children[index]) cur->children[index] = new TrieNode();
-    cur = cur->children[index];
+    if (!cur->next[index]) cur->next[index] = new TrieNode(key[i]);
+    cur = cur->next[index];
   }
   cur->isEndOfWord = true;
 }
 
-void emptyTrieTree(TrieNode* node) {}
+void emptyTrieTree(TrieNode* root) {
+  if (!root) return;
 
-void dfsPrintTrieTreeUtil(const TrieNode* node, int val) {
-  if (!node) return;
+  std::queue<TrieNode*> to_be_visited;
+  for (int i = 0; i < kAlphabetSize; ++i) {
+    if (root->next[i]) to_be_visited.push(root->next[i]);
+  }
+  delete root;
+  root = NULL;
 
-  std::cout << char('a' + val) << " ";
-  if (node->isEndOfWord) std::cout << std::endl;
+  while (!to_be_visited.empty()) {
+    auto front = to_be_visited.front();
+    to_be_visited.pop();
 
-  for (int i = 0; i < kAlphabetSize; ++i)
-    dfsPrintTrieTreeUtil(node->children[i], i);
+    for (int i = 0; i < kAlphabetSize; ++i) {
+      if (front->next[i]) to_be_visited.push(front->next[i]);
+    }
+    delete front;
+    front = NULL;
+  }
 }
 
-void dfsPrintTrieTree(const TrieNode* root) {
+void dfsPrintTrieTree(const TrieNode* node, std::string path) {
+  // print root to leaf path.
+
+  if (!node) return;
+  if (node->isEndOfWord)
+    std::cout << path.substr(1, path.length() - 1) + node->val << std::endl;
+
   for (int i = 0; i < kAlphabetSize; ++i)
-    dfsPrintTrieTreeUtil(root->children[i], i);
+    dfsPrintTrieTree(node->next[i], path + node->val);
 }
 
 void bfsPrintTrieTree(const TrieNode* root) {
@@ -54,24 +66,17 @@ void bfsPrintTrieTree(const TrieNode* root) {
 
   std::queue<TrieNode*> to_be_visited;
   for (int i = 0; i < kAlphabetSize; ++i) {
-    if (root->children[i]) {
-      std::cout << char('a' + i) << " ";
-      to_be_visited.push(root->children[i]);
-    }
+    if (root->next[i]) to_be_visited.push(root->next[i]);
   }
-  std::cout << std::endl;
   int count = to_be_visited.size();
 
   while (!to_be_visited.empty()) {
     for (int i = 0; i < count; ++i) {
       auto front = to_be_visited.front();
       to_be_visited.pop();
-
+      std::cout << front->val << " ";
       for (int j = 0; j < kAlphabetSize; ++j) {
-        if (front->children[j]) {
-          std::cout << char('a' + j) << " ";
-          to_be_visited.push(front->children[j]);
-        }
+        if (front->next[j]) to_be_visited.push(front->next[j]);
       }
     }
     std::cout << std::endl;
@@ -83,9 +88,9 @@ bool searchTrie(const TrieNode* root, std::string key) {
   const TrieNode* pCur = root;
   for (int i = 0; i < key.length(); ++i) {
     int idx = key[i] - 'a';
-    if (!pCur->children[idx]) return false;
+    if (!pCur->next[idx]) return false;
 
-    pCur = pCur->children[idx];
+    pCur = pCur->next[idx];
   }
   return pCur != NULL && pCur->isEndOfWord;
 }
@@ -96,14 +101,16 @@ std::string searchPrefix(const TrieNode* root, const std::string word) {
   std::string result = "";
   for (int i = 0; i < word.length(); ++i) {
     int idx = word[i] - 'a';
-    if (!pCur->children[idx]) {
+    if (!pCur->next[idx]) {
       if (pCur->isEndOfWord)
-        break;
+        return result;
       else
         return "";
+    } else {
+      if (pCur->isEndOfWord) return result;
     }
 
-    pCur = pCur->children[idx];
+    pCur = pCur->next[idx];
     result += word[i];
   }
 
@@ -112,17 +119,18 @@ std::string searchPrefix(const TrieNode* root, const std::string word) {
 
 std::string replaceWords(const std::vector<std::string>& dict,
                          const std::string sentence) {
-  auto append_a_word = [](
+  auto append_a_word = [&](
       const TrieNode* root, const std::string word, std::string& ans) {
     std::string prefix = searchPrefix(root, word);
     ans += (prefix.empty() ? word : prefix);
   };
 
   // build the TrieTree.
-  TrieNode* root = new TrieNode();
+  // root is a special node whose value is '#'
+  TrieNode* root = new TrieNode('#');
   for (auto word : dict) insertTrie(root, word);
   // bfsPrintTrieTree(root);
-  // dfsPrintTrieTree(root);
+  // dfsPrintTrieTree(root, "");
 
   size_t start = 0;
   std::string ans = "";
@@ -141,12 +149,18 @@ std::string replaceWords(const std::vector<std::string>& dict,
     std::string word = sentence.substr(start, sentence.length() - start);
     append_a_word(root, word, ans);
   }
+
+  emptyTrieTree(root);
   return ans;
 }
 
 int main() {
-  std::vector<std::string> dict = {"cat", "bat", "rat"};
-  std::string sentence = "the cattle was rattled by the battery";
+  // std::vector<std::string> dict = {"cat", "bat", "rat"};
+  // std::string sentence = "the cattle was rattled by the battery";
+
+  // expected : "a a a a a a a a bbb baba a"
+  std::vector<std::string> dict = {"a", "aa", "aaa", "aaaa"};
+  std::string sentence = "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa";
 
   std::string result = replaceWords(dict, sentence);
 
